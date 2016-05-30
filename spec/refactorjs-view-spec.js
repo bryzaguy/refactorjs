450e726e3c2af8117ae87f79ad156d449d9f487c
Bryant Syme <bsyme@newrelic.com>
2016-05-30 11:38:00 -0700

    Init

---
+++ spec/refactorjs-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import RefactorjsView from '../lib/refactorjs-view';
+
+describe('RefactorjsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
